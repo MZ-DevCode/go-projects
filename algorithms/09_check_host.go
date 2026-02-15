79952c237afe7676a4e66f11804bb3c4d75fdcb4
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-02-15 23:24:31 +0100

    update

--- algorithms/09_check_host.go
+++ algorithms/09_check_host.go
@@ -8,13 +8,10 @@ import (
 
 func main() {
 	var url string
-
 	fmt.Print("Enter target URL: ")
 	fmt.Scan(&url)
 
-	client := http.Client{
-		Timeout: 5 * time.Second,
-	}
+	client := http.Client{Timeout: 5 * time.Second}
 
 	resp, err := client.Get(url)
 	if err != nil {
@@ -24,6 +21,10 @@ func main() {
 	defer resp.Body.Close()
 
 	fmt.Printf("\n[+] Target: %s\n", url)
-	fmt.Printf("Status Code: %d\n", resp.StatusCode)
-	fmt.Printf("Server Info: %s\n", resp.Header.Get("Server"))
+	fmt.Printf("Status: %d\n", resp.StatusCode)
+	fmt.Printf("Server: %s\n", resp.Header.Get("Server"))
+	fmt.Printf("Powered By: %s\n", resp.Header.Get("X-Powered-By"))
+	fmt.Printf("CSP Security: %s\n", resp.Header.Get("Content-Security-Policy"))
+	fmt.Printf("HSTS (HTTPS Only): %s\n", resp.Header.Get("Strict-Transport-Security"))
+	fmt.Printf("Cookies: %s\n", resp.Header.Get("Set-Cookie"))
 }
