51eac21c53ff9d49cf733dbdc702368578c163f9
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-03-14 13:35:03 +0100

    update

--- fyne-app/main.go
+++ fyne-app/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/widget"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
@@ -11,12 +12,21 @@ func main() {
 	w := a.NewWindow("Test")
 
 	label1 := widget.NewLabel("Hello World")
+
+	entry := widget.NewEntry()
+	entry.SetPlaceHolder("Enter text...")
+
 	label2 := widget.NewButton("Click Me", func() {
 		label1.SetText("Button Clicked")
+		data := entry.Text
+		fmt.Println(data)
+		label1.SetText(data)
 	})
+
 	w.SetContent(container.NewVBox(
 		label1,
 		label2,
+		entry,
 	))
 	w.ShowAndRun()
 }
