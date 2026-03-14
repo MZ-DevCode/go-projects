b0b2a63fbf01873eb146b1fcc80b8a06b1062236
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-03-14 12:51:41 +0100

    test library fyne button

--- fyne-app/main.go
+++ fyne-app/main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2/container"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Test")
 
+	label1 := widget.NewLabel("Hello World")
+	label2 := widget.NewButton("Click Me", func() {
+		label1.SetText("Button Clicked")
+	})
+	w.SetContent(container.NewVBox(
+		label1,
+		label2,
+	))
 	w.ShowAndRun()
 }
