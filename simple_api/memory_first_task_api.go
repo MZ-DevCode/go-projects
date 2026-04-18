4ef7ac02d0f31e6c9912abe63675023a94bdc673
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-04-18 13:29:47 +0200

    feat: first functional memory-based task api

---
+++ simple_api/memory_first_task_api.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Task struct{
+	ID	int `json:"id"`
+	Content string `json:"content"`
+	Done	bool `json:"done"`
+}
+
+var tasks = []Task{
+	{ID: 1, Content: "Buy groceries", Done: false},
+}
+func main(){
+	mux := http.NewServeMux()
+	mux.HandleFunc("/all", func(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(tasks)
+	})
+
+	mux.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request){
+		val := r.URL.Query().Get("text")
+		newTask := Task{
+			ID: len(tasks) + 1,
+			Content: val,
+			Done: false,
+		}
+		tasks = append(tasks, newTask)
+		fmt.Fprint(w, "Задание добавлено")
+	})
+
+	mux.HandleFunc("/clear", func(w http.ResponseWriter, r *http.Request){
+		tasks = nil
+		fmt.Fprint(w, "Задания очищены")
+	})
+	fmt.Println("Server started on :8080")
+	http.ListenAndServe(":8080", mux)
+
+}
