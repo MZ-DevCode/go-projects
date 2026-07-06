33ec6a8a08bc9ee393bd6cb8de4441ed2e97de29
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-07-06 15:56:53 +0200

    -

---
+++ tests/main.go
@@ -0,0 +1,19 @@
+
+package main
+
+import "fmt"
+
+func Add(a, b int) int{
+	return a + b
+}
+
+func Multiply(a, b int) int{
+	return a * b
+}
+
+func main(){
+	fmt.Println(Add(2,3))
+	fmt.Println(Multiply(2,3))
+}
+
+
---
+++ tests/main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T){
+	got := Add(2,3)
+	want := 5
+	if got != want{
+		t.Errorf("Add(2,3) = %d; want = %d", got, want)
+	}
+}
+
+func TestMultiply(t *testing.T){
+	got := Multiply(2,3)
+	want := 6
+	if got != want{
+		t.Errorf("Multiply(2,3) = %d; want %d", got, want)
+	}
+}
