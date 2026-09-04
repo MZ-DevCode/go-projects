f8e033d5204df3ac7bf5f35950d39c74a148724b
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-09-04 23:14:02 +0200

    func load

--- todo-app/main.go
+++ todo-app/main.go
@@ -6,12 +6,28 @@ import(
 	"strconv"
 )
 
-type Task struct(
-	ID int
-	Text string
-	Status bool
-)
+type Task struct{
+	ID 	int	`json:"id"`
+	Text 	string	`json:"text"`
+	Status 	bool	`json:"status"`
+}
+
+const fileName = "tasks.json"
+
+func loadTasks() []Task{
+	file, err := os.ReadFile(fileName)
+	if err != nil{
+		log.Println("File doesn't exist: ")
+		return []Task{}
+	}
+
+	var tasks []Task
+
+	json.Unmarshal(file, &tasks)
+
+	return tasks
+}
 
 func main(){
-	
+		
 }
