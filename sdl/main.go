70a27c195d6543e60611c42d578e201208929656
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-09-10 19:56:01 +0200

    go-sdl

---
+++ sdl/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+func main() {
+	if err := sdl.Init(sdl.INIT_VIDEO); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+
+	window, err := sdl.CreateWindow("Go SDL2", 100, 100, 800, 600, sdl.WINDOW_SHOWN)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		panic(err)
+	}
+	defer renderer.Destroy()
+
+	running := true
+	for running {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				running = false
+			}
+		}
+
+		renderer.SetDrawColor(30, 30, 30, 255)
+		renderer.Clear()
+		renderer.Present()
+		
+		sdl.Delay(16)
+	}
+}
